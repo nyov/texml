1d9361495e7b911df0fbd58af74c6b8927dbd76a
olpa <olpa@c9bd6c6d-8c10-4832-ae80-b1add196a410>
2004-03-15 13:16:51 +0000

    Adding comments to empty lines is implemented.
    git-svn-id: svn://svn.code.sf.net/p/getfo/code/trunk@98 c9bd6c6d-8c10-4832-ae80-b1add196a410

--- texml/bin/texmlwr.py
+++ texml/bin/texmlwr.py
@@ -1,5 +1,5 @@
 """ TeXML Writer and string services """
-# $Id: texmlwr.py,v 1.1 2004-03-15 12:16:53 olpa Exp $
+# $Id: texmlwr.py,v 1.2 2004-03-15 13:16:51 olpa Exp $
 
 #
 # Modes of processing of special characters
@@ -34,15 +34,57 @@ class texmlwr:
   def __init__(self, stream):
     """ Remember output stream, initialize data structures """
     self.stream = stream
-    self.after_ch09   = 0;
-    self.after_ch0a   = 0;
+    self.after_char0d = 1;
+    self.after_char0a = 1;
     self.mode         = TEXT;
     self.mode_stack   = [];
-    self.stream = stream
+    self.stream       = stream
     self.stream_stack = [];
+
+  def stackMode(self, mode):
+    """ put new mode into the stack of modes """
+    self.mode_stack.extend(mode)
+    if mode != DEFAULT:
+      self.mode = mode
+
+  def unstackMode(self):
+    self.mode = self.mode_stack.pop()
   
   def writech(self, ch, esc_specials):
     """ Write a char, (maybe) escaping specials """
+    #
+    # Handle end-of-line symbols in special way
+    # We automagically process "\n", "\r", "\n\r" and "\r\n" line ends
+    #
+    if ('\n' == ch) or ('\r' == ch):
+      #
+      # line end symbol of the same type starts new line
+      #
+      if (('\n' == ch) and self.after_char0a) or (('\r' == ch) and self.after_char0d):
+        self.stream.write('%')
+      #
+      # Two different line end symbols clean each other
+      #
+      if self.after_char0a and self.after_char0d:
+        self.after_char0a = 0
+        self.after_char0d = 0
+      #
+      # Remember that end of line has happended
+      #
+      if '\n' == ch:
+        self.after_char0a = 1
+      else:
+        self.after_char0d = 1
+      #
+      # Write line end char and return
+      #
+      self.stream.write(ch)
+      return                                         # return
+    #
+    # Not end-of-line symbol
+    #
+    self.after_char0d = 0
+    self.after_char0a = 0
     if not esc_specials:
       self.stream.write(ch)
     else:
