95737f924c7d345a0ed7fc0d9ed04e0bef7b23f4
olpa <olpa@c9bd6c6d-8c10-4832-ae80-b1add196a410>
2004-03-16 11:39:04 +0000

    Initial support for "cmd" (and "opt and "parm").
    git-svn-id: svn://svn.code.sf.net/p/getfo/code/trunk@111 c9bd6c6d-8c10-4832-ae80-b1add196a410

--- texml/bin/handler.py
+++ texml/bin/handler.py
@@ -1,18 +1,36 @@
 """ Tranform TeXML SAX stream """
-# $Id: handler.py,v 1.3 2004-03-15 13:50:18 olpa Exp $
+# $Id: handler.py,v 1.4 2004-03-16 11:39:04 olpa Exp $
 
 import xml.sax.handler
 import texmlwr
+import StringIO
 
 class handler(xml.sax.handler.ContentHandler):
 
+  # Object variables
+  # writer
+  # 
+  # For <cmd /> support:
+  # parm_content
+  # opt_content
+  # parm_stack
+  # opt_stack
+  # cmdname
+  # cmdname_stack
+
   def __init__(self, stream):
     """ Create writer, create maps """
-    self.writer = texmlwr.texmlwr(stream)
+    self.writer        = texmlwr.texmlwr(stream)
+    self.parm_stack    = []
+    self.opt_stack     = []
+    self.cmdname_stack = []
+    self.parm_content  = ''
+    self.opt_content   = ''
+    self.cmdname       = ''
     #
     # Create handler maps
     #
-    self.model_nomath  = {
+    self.model_nomath = {
       'TeXML':  self.on_texml,
       'cmd':    self.on_cmd,
       'env':    self.on_env,
@@ -23,16 +41,17 @@ class handler(xml.sax.handler.ContentHandler):
     self.model_content          = self.model_nomath.copy();
     self.model_content['math']  = self.on_math;
     self.model_content['dmath'] = self.on_dmath;
-    self.model_cmd = {
+    self.model_cmd    = {
       'opt':    self.on_opt,
       'parm':   self.on_parm
     }
-    self.model_opt = {
+    self.model_opt    = {
       'spec':   self.on_spec,
       'ctrl':   self.on_ctrl,
       'cmd':    self.on_cmd,
       'math':   self.on_math
     }
+    self.model_parm   = self.model_opt
     self.end_handlers = {
       'TeXML':  self.on_texml_end,
       'cmd':    self.on_cmd_end,
@@ -78,7 +97,7 @@ class handler(xml.sax.handler.ContentHandler):
     """ Restore content model of parent """
     self.model = self.model_stack.pop()
 
-  # -------------------------------------------------------------------
+  # -----------------------------------------------------------------
 
   def on_texml(self, attrs):
     """ Handle TeXML element """
@@ -101,11 +120,85 @@ class handler(xml.sax.handler.ContentHandler):
     """ Handle TeXML element. Restore old mode. """
     self.writer.unstack_mode()
 
-  def on_cmd(self):
-    pass
+  # -----------------------------------------------------------------
+
+  def on_cmd(self, attrs):
+    """ Handle 'cmd' element """
+    self.stack_model(self.model_cmd)
+    #
+    # Get name of the command
+    #
+    name = attrs.get('name', '')
+    if 0 == len(name):
+      raise ValueError('Attribute cmd/@name is empty')
+    self.cmdname_stack.append(self.cmdname)
+    self.cmdname = name
+    #
+    # Setup output streams and stacks
+    #
+    self.writer.stack_stream(StringIO.StringIO())
+    self.parm_stack.append(self.parm_content)
+    self.opt_stack.append(self.opt_content)
+    self.parm_content = '';
+    self.opt_content  = '';
 
   def on_cmd_end(self):
-    pass
+    #
+    # Get name, text content, "parm" and "opt" strings
+    # Restore old values.
+    #
+    chars = self.writer.unstack_stream()
+    name  = self.cmdname
+    parm  = self.parm_content
+    opt   = self.opt_content
+    self.cmdname      = self.cmdname_stack.pop()
+    self.parm_content = self.parm_stack.pop()
+    self.opt_content  = self.opt_stack.pop()
+    #
+    # Check that chars inside are only spaces (comment char also ok)
+    #
+    if 0 != len(chars.strip(" \t\n\r\f\v%")):
+      raise ValueError("Element 'cmd' has text content: '%s'" % chars.encode('latin-1', 'replace'))
+    #
+    # And now write command
+    #
+    self.writer.writech('\\', 0)
+    if (0 == len(parm)) and (0 == len(opt)):
+      self.stream.write(name, 0)
+      self.writer.writech(' ', 0)
+    else:
+      self.writer.write(name, 0)
+      if 0 != len(opt):
+        self.writer.writech('[', 0)
+        self.writer.write(opt)
+        self.writer.writech(']', 0)
+      if 0 != len(parm):
+        self.writer.writech('{', 0)
+        self.writer.write(parm)
+        self.writer.writech('}', 0)
+
+  def on_opt(self, attrs):
+    """ Handle 'opt' element """
+    self.on_opt_and_parm(self.opt_content)
+
+  def on_parm(self, attrs):
+    """ Handle 'parm' element """
+    self.on_opt_and_parm(self.parm_content)
+
+  def on_opt_and_parm(self, str):
+    self.stack_model(self.model_opt)
+    if 0 != len(str):
+      self.writer.stack_stream(StringIO.StringIO(str + ','))
+    else:
+      self.writer.stack_stream(StringIO.StringIO())
+ 
+  def on_opt_end(self):
+    self.opt_content = self.writer.unstack_stream()
+
+  def on_parm_end(self):
+    self.parm_content = self.writer.unstack_stream()
+
+  # -----------------------------------------------------------------
 
   def on_env(self):
     pass
@@ -129,18 +222,6 @@ class handler(xml.sax.handler.ContentHandler):
     pass
    
   def on_spec_end(self):
-    pass
- 
-  def on_opt(self):
-    pass
- 
-  def on_opt_end(self):
-    pass
-
-  def on_parm(self):
-    pass
-
-  def on_parm_end(self):
     pass
 
   def on_math(self, attrs):
