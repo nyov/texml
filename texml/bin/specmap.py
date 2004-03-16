3104312cdfa7f6e1a87bc7270d7ea22c0d340866
olpa <olpa@c9bd6c6d-8c10-4832-ae80-b1add196a410>
2004-03-16 15:10:42 +0000

    Map from category name to symbol. &nbsp; and &copy; are added to specials.
    git-svn-id: svn://svn.code.sf.net/p/getfo/code/trunk@120 c9bd6c6d-8c10-4832-ae80-b1add196a410

--- texml/bin/specmap.py
+++ texml/bin/specmap.py
@@ -1,5 +1,5 @@
 """ Map special TeX symbols """
-# $Id: specmap.py,v 1.2 2004-03-15 13:14:28 olpa Exp $
+# $Id: specmap.py,v 1.3 2004-03-16 15:10:42 olpa Exp $
 
 # text escape map and map escape map should contain the same keys
 
@@ -16,7 +16,9 @@ textescmap = {
   '%':  r'\%',
   '|':  r'$|$',
   '<':  r'$<$',
-  '>':  r'$>$'
+  '>':  r'$>$',
+  # not special but typography
+  u'\u00a9': r'\textcopyright '
 }
 
 mathescmap = {
@@ -32,6 +34,37 @@ mathescmap = {
   '%':  r'\%',
   '|':  r'|',
   '<':  r'<',
-  '>':  r'>'
+  '>':  r'>',
+  # not special but typography
+  u'\u00a9': r'\copyright '
 }
 
+#
+# Although these symbols are not special, it is better to escape them
+# because in as-is form they are not so good
+#
+typographymap = {
+  u'\u00a0':  r'~'
+}
+
+textescmap.update(typographymap)
+mathescmap.update(typographymap)
+
+#
+# Mapping from spec/@cat to symbols
+#
+tocharmap = {
+  'esc':     '\\',
+  'bg':      '{',
+  'eg':      '}',
+  'mshift':  '$',
+  'align':   '&',
+  'parm':    '#',
+  'sup':     '^',
+  'sub':     '_',
+  'tilde':   '~',
+  'comment': '%',
+  'vert':    '|',
+  'lt':      '<',
+  'gt':      '>'
+}
