dde773db626720203d1e57b5d59f6a702453daf4
olpa <olpa@c9bd6c6d-8c10-4832-ae80-b1add196a410>
2004-06-28 12:43:19 +0000

    Initial.
    git-svn-id: svn://svn.code.sf.net/p/getfo/code/trunk@345 c9bd6c6d-8c10-4832-ae80-b1add196a410

---
+++ texml/tests/data/quick/quick.bat
@@ -0,0 +1,5 @@
+REM In c:\texml\bin folder use this:
+REM python texml.py quick.texml quick.tex
+REM In c:\texml\tests\data\quick folder use this:
+python ../../../bin/texml.py quick.texml quick.tex
+pause
